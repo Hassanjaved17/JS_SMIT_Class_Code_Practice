11605515f8877632fa16bcb793827041843410ea
Hassanjaved17 <hassanaptech9@gmail.com>
2026-01-24 23:16:25 +0500

    practice objects and its methods

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ console.log("server is running");
 // ];
 
 
-// console.log("twoDArray",twoDArray);
+    // console.log("twoDArray",twoDArray);
 
 // ------practice end-----------
 
@@ -24,6 +24,8 @@ console.log("server is running");
 // 08-dec-25 class code practice
 
 // ======= (1) ========
+// Take user name and slice first 3 letters
+
 // var myName = prompt("Enter your name");
 // var myNameLenght = myName.length;
 
@@ -34,6 +36,7 @@ console.log("server is running");
 // }
 
 // ======= (2) ========
+// Check if user typed double space (" ")
 
 // checking double space entered  by user
 // var cityName = prompt("Enter your city name");
@@ -51,14 +54,84 @@ console.log("server is running");
 // }
 
 // ======= (3) ========
+// Replace "World War II" with "The second World War" manually
 
-var cityName = "This is World War II story of the Past World War II";
+// var cityName = "This is World War II story of the Past World War II";
 
-var cityNames = cityName.length;
-console.log(cityNames)
-for (i = 0; i < cityNames; i++) {
-    if (cityName.slice(i, i + 12) === "World War II") {
-        cityName = cityName.slice(0, i) + "The Second World War" + cityName.slice(i + 12);
-        console.log("CityName =>", cityName);
-    }
+// var cityNames = cityName.length;
+// console.log(cityNames)
+// for (i = 0; i < cityNames; i++) {
+//     if (cityName.slice(i, i + 12) === "World War II") {
+//         cityName = cityName.slice(0, i) + "The Second World War" + cityName.slice(i + 12);
+//         console.log("CityName =>", cityName);
+//     }
+// }
+
+// ======= (4) ========
+//  indexOf example
+
+// var authorName = "hassan";
+// var checkName = authorName.indexOf("s");
+// console.log(checkName);
+
+// ======= (5) ========
+// lastIndexOf + replace last occurrence
+
+// var sentence = "Pakistan is the beautiful country and Pakistan also."
+// var sentences = sentence.lastIndexOf("Pakistan");
+
+// if(sentence !== -1){
+//     var result = sentence.slice(0, sentences) + "Turkey" + sentence.slice(sentences + 8);
+//     console.log(result);
+// }
+
+
+// let checkCities = ["karachi", "lahore", "islamabad", "quetta", "peshawar"];
+
+// console.log(checkCities.length,checkCities);
+
+// checkCities.push("multan");
+// checkCities.unshift("faizabad");
+// checkCities.shift("faizabad");
+// console.log(checkCities)
+
+
+// console.log(checkCities.length,checkCities);    
+
+// let fruits = ["apple", "banana", "orange", "mango", "grapes"];
+
+// let selectedFruits = fruits.slice(1,4);
+// console.log("selectedFruits",selectedFruits);
+// console.log("fruits",fruits);
+
+// let cities = ["karachi", "lahore", "islamabad", "quetta", "peshawar"];
+
+// let removedCities = cities.splice(2,2,"faizabad","sialkot");
+// console.log("removedCities",removedCities);
+// console.log("cities",cities);
+
+// let arr = [1,2,3,4,5];
+// let result = arr.concat([6,7,8,,9,10]);
+// console.log("result",result);
+// console.log("arr",arr);
+
+// let fName = "james";
+// let lNmae = "bond";
+// let fullName = fName + " " + lNmae;
+// console.log(fullName);
+
+
+// Js Objects 
+
+let student ={
+    name: "hassan",
+    age: 25,
+    city: "karachi",
+    isMarried: false,
 }
+
+// accessing object properties
+console.log(student.name);
+student.address = "abc street";
+console.log(student)
+
