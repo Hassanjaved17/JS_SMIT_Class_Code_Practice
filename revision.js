a9d123bcedcd88eb061b71179722b9f2aa3a2ca5
Hassanjaved17 <hassanaptech9@gmail.com>
2026-03-01 22:47:41 +0500

    feat: add initial HTML and JavaScript files for JS revision

---
+++ revision.js
@@ -0,0 +1,18 @@
+console.log("Js is running")
+
+// variables in js the difference between var, let and const are that var is function scoped and can be re-declared and updated, let is block scoped and can be updated but not re-declared, const is block scoped and cannot be updated or re-declared. 
+// var is used to declare a variable that can be re-declared and updated, let is used to declare a variable that can be updated but not re-declared, const is used to declare a variable that cannot be updated or re-declared.
+
+// var a = 10;
+// var b = 20;
+// var c = a + b;
+// console.log(c);
+
+// let x = 10;
+// x = 20;
+// console.log(x); 
+
+var bolean = true;
+var a = 10;
+var c = a + bolean;
+console.log(c); // 11 because true is treated as 1 in arithmetic operations.
