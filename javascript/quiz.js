5ad00fbcf0791224f19da08513e873ef9aef747c
Hassanjaved17 <hassanaptech9@gmail.com>
2026-03-31 22:51:19 +0500

    practice js quiz

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -1,12 +1,35 @@
-// Q1 Changing Case
-var uname = "hassan";
-var upperCaseName = uname.toUpperCase();
-console.log(upperCaseName); // Output: "HASSAN"
+// // Q1 Changing Case
+// var uname = "hassan";
+// var upperCaseName = uname.toUpperCase();
+// console.log(upperCaseName); // Output: "HASSAN"
 
-// Q2 String Length & Extracting
-var sentence = "The quick brown fox jumps over the lazy dog.";
-var word = "fox";
-var startIndex = sentence.indexOf(word);
-var endIndex = startIndex + word.length;
-var extractedWord = sentence.slice(startIndex, endIndex);
-console.log(extractedWord); // Output: "fox"
+// // Q2 String Length & Extracting
+// var sentence = "The quick brown fox jumps over the lazy dog.";
+// var word = "fox";
+// var startIndex = sentence.indexOf(word);
+// var endIndex = startIndex + word.length;
+// var extractedWord = sentence.slice(startIndex, endIndex);
+// console.log(extractedWord); // Output: "fox"
+
+// // Q3 Finding Segments
+// var str = "JavaScript is a powerful programming language.";
+// console.log(str.indexOf("Script")); // Output: 4
+// console.log(str.indexOf("Java"));   // Output: 0
+// console.log(str.indexOf("Python")); // Output: -1
+
+// Q4 Character at Position
+// var str = "Hello, World!";
+// console.log(str.charAt(0));
+// console.log(str.charAt(7));
+// console.log(str.charAt(4));
+
+// Q5 Replacing Characters
+// var str = "I love JavaScript!";
+// var newStr = str.replace("JavaScript", "JS");
+// console.log(newStr); // Output: "I love JS!" 
+
+// Q6 Rounding Numbers
+// var num1 = 3.7;     
+// var num2 = 3.2;
+// console.log(Math.round(num1)); // Output: 4
+// console.log(Math.round(num2)); // Output: 3
