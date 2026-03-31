2137dc0b4622f7606787df6a292782d961c9cc54
Hassanjaved17 <hassanaptech9@gmail.com>
2026-03-31 22:27:48 +0500

    practice Js Quiz

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -4,3 +4,9 @@ var upperCaseName = uname.toUpperCase();
 console.log(upperCaseName); // Output: "HASSAN"
 
 // Q2 String Length & Extracting
+var sentence = "The quick brown fox jumps over the lazy dog.";
+var word = "fox";
+var startIndex = sentence.indexOf(word);
+var endIndex = startIndex + word.length;
+var extractedWord = sentence.slice(startIndex, endIndex);
+console.log(extractedWord); // Output: "fox"
