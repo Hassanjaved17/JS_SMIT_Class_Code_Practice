9661f7ecb1c8296c88908d91f251473ab8d8ad24
Hassanjaved17 <hassanaptech9@gmail.com>
2026-04-14 20:48:48 +0500

    add datatypes.html and datatypes.js for data types demonstration

---
+++ javascript/datatypes.js
@@ -0,0 +1,22 @@
+console.log("server is running");
+
+// Data types in js
+// 1. String
+var uname = "hassan";
+console.log(`uname=> ${uname}`); 
+
+// 2. Number
+var age = 20;
+console.log(`age=> ${age}`);
+
+// 3. Boolean
+var isLoggedIn = true;
+console.log(`isLoggedIn=> ${isLoggedIn}`);
+
+// 4. null
+var user = null;
+console.log(`user=> ${user}`);
+
+// 5. undefined
+var city;
+console.log(`city=> ${city}`);
--- javascript/revision.js
+++ javascript/revision.js
@@ -87,4 +87,6 @@ console.log("Js is running")
 // document.getElementById("demo").innerHTML = "JavaScript is a programming language that is commonly used to create interactive effects within web browsers. It is a client-side scripting language, which means that it runs on the user's computer rather than on the server. JavaScript can be used to create dynamic web pages, validate forms, create animations, and much more. It is an essential part of modern web development and is supported by all major web browsers."
 
 
+
+
    
