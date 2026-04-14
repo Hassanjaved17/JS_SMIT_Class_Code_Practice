14c749aa78ac592c4086de204739239fa3703acb
Hassanjaved17 <hassanaptech9@gmail.com>
2026-04-14 20:52:02 +0500

    add additional data types examples in datatypes.js

--- javascript/datatypes.js
+++ javascript/datatypes.js
@@ -20,3 +20,64 @@ console.log(`user=> ${user}`);
 // 5. undefined
 var city;
 console.log(`city=> ${city}`);
+
+// 6. Object
+var person = {
+    name: "hassan",
+    age: 20,
+    city: "karachi"
+}
+console.log(`person=> ${person}`);
+
+// // 7. Symbol
+// var id = Symbol("id");
+// console.log(`id=> ${id}`);
+
+// 8. BigInt
+var bigNumber = 1234567890123456789012345678901234567890n;
+console.log(`bigNumber=> ${bigNumber}`);
+
+// 9. Function
+var add = function(a, b) {
+    return a + b;
+}
+console.log(`add=> ${add}`);
+
+// 10. Array
+var numbers = [1, 2, 3, 4, 5];
+console.log(`numbers=> ${numbers}`);
+
+// 11. Date
+var date = new Date();
+console.log(`date=> ${date}`);
+
+// 12. RegExp
+var regex = /hello/;
+console.log(`regex=> ${regex}`);
+
+// 13. Set
+var set = new Set([1, 2, 3, 4, 5]);
+console.log(`set=> ${set}`);
+
+// 14. Map
+var map = new Map();
+map.set("name", "hassan");
+map.set("age", 20);
+console.log(`map=> ${map}`);
+
+// 15. Proxy
+var obj = new Proxy({}, {
+    get: function(target, prop) {
+        return target[prop];
+    },
+    set: function(target, prop, value) {
+        target[prop] = value;
+    }
+});
+console.log(`obj=> ${obj}`);
+
+// 16. Promise
+var promise = new Promise(function(resolve, reject) {
+    resolve("success");
+});
+console.log(`promise=> ${promise}`);
