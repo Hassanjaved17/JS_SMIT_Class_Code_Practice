a7c2b4e58b4635e3874ace34882337f3f76283b3
Hassanjaved17 <hassanaptech9@gmail.com>
2026-04-16 21:43:02 +0500

    "practice variables in js"

--- javascript/datatypes.js
+++ javascript/datatypes.js
@@ -5,79 +5,79 @@ console.log("server is running");
 var uname = "hassan";
 console.log(`uname=> ${uname}`); 
 
-// 2. Number
-var age = 20;
-console.log(`age=> ${age}`);
+// // 2. Number
+// var age = 20;
+// console.log(`age=> ${age}`);
 
-// 3. Boolean
-var isLoggedIn = true;
-console.log(`isLoggedIn=> ${isLoggedIn}`);
+// // 3. Boolean
+// var isLoggedIn = true;
+// console.log(`isLoggedIn=> ${isLoggedIn}`);
 
-// 4. null
-var user = null;
-console.log(`user=> ${user}`);
+// // 4. null
+// var user = null;
+// console.log(`user=> ${user}`);
 
-// 5. undefined
-var city;
-console.log(`city=> ${city}`);
+// // 5. undefined
+// var city;
+// console.log(`city=> ${city}`);
 
-// 6. Object
-var person = {
-    name: "hassan",
-    age: 20,
-    city: "karachi"
-}
-console.log(`person=> ${person}`);
+// // 6. Object
+// var person = {
+//     name: "hassan",
+//     age: 20,
+//     city: "karachi"
+// }
+// console.log(`person=> ${person}`);
 
-// // 7. Symbol
-// var id = Symbol("id");
-// console.log(`id=> ${id}`);
+// // // 7. Symbol
+// // var id = Symbol("id");
+// // console.log(`id=> ${id}`);
 
-// 8. BigInt
-var bigNumber = 1234567890123456789012345678901234567890n;
-console.log(`bigNumber=> ${bigNumber}`);
+// // 8. BigInt
+// var bigNumber = 1234567890123456789012345678901234567890n;
+// console.log(`bigNumber=> ${bigNumber}`);
 
-// 9. Function
-var add = function(a, b) {
-    return a + b;
-}
-console.log(`add=> ${add}`);
+// // 9. Function
+// var add = function(a, b) {
+//     return a + b;
+// }
+// console.log(`add=> ${add}`);
 
-// 10. Array
-var numbers = [1, 2, 3, 4, 5];
-console.log(`numbers=> ${numbers}`);
+// // 10. Array
+// var numbers = [1, 2, 3, 4, 5];
+// console.log(`numbers=> ${numbers}`);
 
-// 11. Date
-var date = new Date();
-console.log(`date=> ${date}`);
+// // 11. Date
+// var date = new Date();
+// console.log(`date=> ${date}`);
 
-// 12. RegExp
-var regex = /hello/;
-console.log(`regex=> ${regex}`);
+// // 12. RegExp
+// var regex = /hello/;
+// console.log(`regex=> ${regex}`);
 
-// 13. Set
-var set = new Set([1, 2, 3, 4, 5]);
-console.log(`set=> ${set}`);
+// // 13. Set
+// var set = new Set([1, 2, 3, 4, 5]);
+// console.log(`set=> ${set}`);
 
-// 14. Map
-var map = new Map();
-map.set("name", "hassan");
-map.set("age", 20);
-console.log(`map=> ${map}`);
+// // 14. Map
+// var map = new Map();
+// map.set("name", "hassan");
+// map.set("age", 20);
+// console.log(`map=> ${map}`);
 
-// 15. Proxy
-var obj = new Proxy({}, {
-    get: function(target, prop) {
-        return target[prop];
-    },
-    set: function(target, prop, value) {
-        target[prop] = value;
-    }
-});
-console.log(`obj=> ${obj}`);
+// // 15. Proxy
+// var obj = new Proxy({}, {
+//     get: function(target, prop) {
+//         return target[prop];
+//     },
+//     set: function(target, prop, value) {
+//         target[prop] = value;
+//     }
+// });
+// console.log(`obj=> ${obj}`);
 
-// 16. Promise
-var promise = new Promise(function(resolve, reject) {
-    resolve("success");
-});
-console.log(`promise=> ${promise}`);
+// // 16. Promise
+// var promise = new Promise(function(resolve, reject) {
+//     resolve("success");
+// });
+// console.log(`promise=> ${promise}`);
