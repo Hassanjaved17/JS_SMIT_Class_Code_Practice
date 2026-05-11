c57d4f247f55cc352c7eee39a5d8528e905375b0
Hassanjaved17 <hassanaptech9@gmail.com>
2026-05-11 19:12:13 +0500

    "practice js"

--- javascript/interview.js
+++ javascript/interview.js
@@ -4,22 +4,35 @@ console.log("Server Is running");
 
 // Ans) var is function-scoped and hoisted to top of function. let and const are block-scoped ({}). const cannot be reassigned. Use const by default, let when you need to change the value, avoid var.
 
- Ex: 
-
+// Ex: 
+//   var x = 10;
+//     let y = 20;
+//     const z = 30;
+//     console.log(x);
 
 // 2. What is a closure in JavaScript?
 
 // Ans) A closure is a function that has access to its own scope, the outer function's scope, and the global scope. It allows a function to access variables from an enclosing scope even after that scope has finished executing.
 
-Ex: function outer() {
-    var x = 10;
-    function inner() {
-        console.log(x);
-    }
-    return inner;
-}
-var myClosure = outer();
-myClosure();
+// Ex:
+//  function outer() {
+//     var x = 10;
+//     function inner() {
+//         console.log(x);
+//     }
+//     return inner;
+// }
+// var myClosure = outer();
+// myClosure();
+
+
+// Q) What is hoisting in JavaScript?
+
+// Ans) Hoisting is a JavaScript mechanism where variable and function declarations are moved to the top of their containing scope during the compilation phase. This means that you can use variables and functions before they are declared in the code. However, only the declarations are hoisted, not the initializations.
+
+// example:
+ console.log(a); // undefined
+var a = 5;
  
 // 3. What is the event loop in JavaScript?
 
